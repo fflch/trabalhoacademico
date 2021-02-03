3de4fe8caffa9dc2bcc213a3ebbb2477696096b5
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-02-03 14:07:10 -0300

    Ajuste - Email

--- resources/views/emails/em_avaliacao.blade.php
+++ resources/views/emails/em_avaliacao.blade.php
@@ -1,12 +1,11 @@
-Olá, professor(a) {{$agendamento->nome_do_orientador}},
+Prezado(a) {{$agendamento->nome_do_orientador}},
 
-Você tem um novo trabalho acadêmico para avaliar:
+{{$agendamento->user->name}} enviou via sistema o TGI "{{$agendamento->titulo}}" e para a publicação é necessário sua aprovação.
 
 <h3><b>Dados do trabalho acadêmico</b></h3>
-<b>Título:</b> {{$agendamento->titulo}}</br>
-<b>Data:</b> {{ Carbon\Carbon::parse($agendamento->data_da_defesa)->format('d/m/Y') }}</br></br>
+<b>Título:</b><a href="trabalhoacademico.fflch.usp.br/agendamentos/$agendamento->id"> {{$agendamento->titulo}}</a></br>
+<b>Data da Defesa:</b> {{ Carbon\Carbon::parse($agendamento->data_da_defesa)->format('d/m/Y') }}</br></br>
 <b>Nome:</b> {{$agendamento->user->name}}
 <b>E-mail USP:</b> {{$agendamento->user->email }} </br>
-
 
 <h4><b>Sistema - Trabalhos Acadêmicos - FFLCH</b></h4>
