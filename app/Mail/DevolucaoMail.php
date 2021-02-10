4dd241d4de4d3fd86531061a549a04ab0d2294e2
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-02-10 16:00:55 -0300

    Anexo de TGIs no Email

--- app/Mail/DevolucaoMail.php
+++ app/Mail/DevolucaoMail.php
@@ -32,10 +32,14 @@ class DevolucaoMail extends Mailable
     public function build()
     {
         $subject = "Devolução de avaliação do trabalho acadêmico de {$this->agendamento->user->name}";
+        $file = File::where('agendamento_id',$this->agendamento->id)->first();
 
         return $this->view('emails.devolver_avaliacao')
         ->to(Pessoa::emailusp($this->agendamento->user->codpes))
         ->subject($subject)
+        ->attachFromStorage($file->path, $file->original_name, [
+            'mime' => 'application/pdf',
+        ])
         ->with([
             'agendamento' => $this->agendamento,
         ]);
