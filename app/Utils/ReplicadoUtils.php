45c336b109064d3038f56ab8ce5dc3f909f25671
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-02-26 15:53:39 -0300

    fix docente aposentado

--- app/Utils/ReplicadoUtils.php
+++ app/Utils/ReplicadoUtils.php
@@ -9,10 +9,8 @@ class ReplicadoUtils {
 
     public static function listarDocentes(){
         $unidade = getenv('REPLICADO_CODUNDCLG');
-        $current = date("Y-m-d H:i:s");
         $query = "SELECT DISTINCT l.codpes, l.nompes from LOCALIZAPESSOA l where l.tipvinext LIKE '%Docente%' and l.codundclg = {$unidade} ORDER BY l.nompes";
-        $docentes = DBreplicado::fetchAll($query);
-        return $docentes;
+        return DBreplicado::fetchAll($query);
     }
 
 } 
